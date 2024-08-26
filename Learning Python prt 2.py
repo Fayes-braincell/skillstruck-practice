ae0043ae0adbbcc1b60bcb17d0bec9caa43c5dc5
Fayes-braincell <favourajagunwade@gmail.com>
2024-08-25 23:15:42 -0700

    exported from Skill Struck

---
+++ Learning Python prt 2.py
@@ -0,0 +1,10 @@
+
+
+
+harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+yummy = harvest.pop(1)
+
+harvest.remove("squash")
+
+print(yummy + " are so delicious if you eat them fresh.")
+print(harvest)

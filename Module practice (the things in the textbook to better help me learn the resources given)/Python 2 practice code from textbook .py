cad01caac490376b7bfa7a7e53882e6971fdc141
Fayes-braincell <favourajagunwade@gmail.com>
2024-09-01 23:46:47 -0700

    exported from Skill Struck

---
+++ Module practice (the things in the textbook to better help me learn the resources given)/Python 2 practice code from textbook .py
@@ -0,0 +1,5 @@
+def weather():
+    print("It's a soggy day outside!")
+
+weather()
+#Other code that have been found under this module are in my notes on paper. Explanations to them are also added.

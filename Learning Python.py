440efd2103de07874fa0d09491b571f6825710b0
Fayes-braincell <favourajagunwade@gmail.com>
2024-09-01 23:46:46 -0700

    exported from Skill Struck

--- Learning Python.py
+++ Learning Python.py
@@ -1,9 +1,1 @@
-first = input("What other item do you want?")
-second = input("What final item do you want?")
 
-treats = ["popcorn", "popsicles", "soda", "chips", "cookies"]
-
-treats.append(first)
-treats.append(second)
-
-print(treats)

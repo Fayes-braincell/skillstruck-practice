6a0c8272a5598a38ef265ccde9faa4f7b7feb933
Fayes-braincell <favourajagunwade@gmail.com>
2024-09-22 23:10:37 -0700

    exported from Skill Struck

---
+++ Testing random code.py
@@ -0,0 +1,20 @@
+
+def practice(skill):
+  print("Today I will practice " + skill)
+  print("I enjoy practicing " + skill)
+
+
+practice("piano")
+practice("soccer")
+practice("karate")
+
+
+classmates = {
+    "Billy" : 8,
+    "Vance" : 12,
+    "Alice" : 10,
+    "Eliza" : 15,
+    "Xavier" : 6,
+}
+
+

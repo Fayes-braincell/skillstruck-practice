10ffbe34b0dffba18a050cab4a52a69dd0f9e910
Fayes-braincell <favourajagunwade@gmail.com>
2024-11-08 22:56:44 -0700

    exported from Skill Struck

--- Python 3/Python 3 Txt file checkup.py
+++ Python 3/Python 3 Txt file checkup.py
@@ -1,8 +1,1 @@
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
 
-p1 = Person("Faye", "17", "Nonbinary")
-print("This is: " + p1.name + "." + " They are: " + p1.age + "." + " They are: " + p1.gender + ".")
